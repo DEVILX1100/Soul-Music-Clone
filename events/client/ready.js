21dc056572df2bc368e6c66991127d46828b45b5
Om Bodkhe <ombbodkhe@gmail.com>
2021-11-04 12:02:49 +0530

    Add files via upload

---
+++ events/client/ready.js
@@ -0,0 +1,12 @@
+module.exports = async (client, message, guild, cmd) => {
+
+  
+      
+
+    
+      console.log("\n")
+      console.log(`[CLIENT] => [READY] ${client.user.tag} IS READY...`.bold.bgWhite.brightMagenta)
+
+}
+
+  
